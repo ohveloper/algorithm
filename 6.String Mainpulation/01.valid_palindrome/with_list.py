d586b93f68a71915b612c158eb4df8f4d527255a
ohveloper <ohveloper@gmail.com>
2021-09-16 09:59:33 +0900

    deque,list

---
+++ 6.String Mainpulation/01.valid_palindrome/with_list.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    # 영문자와 숫자만을 대상으로 한다는 조건을 처리가히 귀한 전처리
+    # 대소문자를 구분하지 않기 때문에 모두 소문자로 변환
+
+    def isPalindrome(self, s: str) -> bool:
+
+        strs = []
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+        # strs 가 비어있지 않았을때
+        # 맨앞의 요소와 맨뒤의 요소를 비교해서 틀리면 false 를 리턴
+
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+
+        return True
+
+
+sol = Solution()
+s = "hello"
+print(sol.isPalindrome(s))
+
+# print("??")
